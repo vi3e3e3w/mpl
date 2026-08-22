625e9cba3beb4861b355661dcae664f2897f580b
vi3e3e3w <levutruonggiang.2013@gmail.com>
2026-08-22 11:22:36 +0700

    Add initial MPL player activity

---
+++ README.md
@@ -0,0 +1,5 @@
+## MPL - Android Music Player with .lrc include
+
+### Hey!
+Everything I have here i use some ChatGPT to code Java...but this idea is mine 
+So don't yap me btw 
--- app/src/main/java/com/giang/mpl/MainActivity.java
+++ app/src/main/java/com/giang/mpl/MainActivity.java
@@ -1,14 +1,46 @@
 package com.giang.mpl;
 
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView songTitle;
+    private Button playButton;
+    private Button pauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        createUserInterface();
+    }
+
+    private void createUserInterface() {
+        LinearLayout layout = new LinearLayout(this);
+
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER);
+        layout.setPadding(32, 32, 32, 32);
+
+        songTitle = new TextView(this);
+        songTitle.setText("No song selected");
+        songTitle.setTextSize(20);
+
+        playButton = new Button(this);
+        playButton.setText("Play");
+
+        pauseButton = new Button(this);
+        pauseButton.setText("Pause");
+
+        layout.addView(songTitle);
+        layout.addView(playButton);
+        layout.addView(pauseButton);
+
+        setContentView(layout);
     }
 }
